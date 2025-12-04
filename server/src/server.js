a30cac3c8d6ac0f2c5515504f7ac1db23362a8fd
tzebohn <tzebohnling@gmail.com>
2025-12-04 17:08:06 -0500

    Update server.js to connect to database before starting Express server

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,15 @@
 import app from "./app.js";
+import { db } from "./db/db.js";
 
 const PORT = 3000
 
-//  Start express server
-app.listen(PORT, () => {
-    console.log(`Server running on port: http://localhost:${PORT}`)
+//  Connect to database
+db.then(() => {
+    console.log('Connected to MySQL!')
+    //  Start express server
+    app.listen(PORT, () => {
+        console.log(`Server running on port: http://localhost:${PORT}`)
+    })
+}).catch(err => {
+    console.error('DB connection error:', err)
 })
