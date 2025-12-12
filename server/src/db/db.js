db0901853055a9aec82c69214181ad5e24efa4c2
tzebohn <tzebohnling@gmail.com>
2025-12-12 08:57:50 -0500

    refactor: switch to MySql connection pool
    - Replaced single DB connection with a pooled connection to support multiple concurrent requests.
    - Updated server.js to use the new MySql pool.

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,11 +1,11 @@
-import mysql from 'mysql2/promise'
+import mysql from 'mysql2'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-})
+}).promise()
--- server/src/server.js
+++ server/src/server.js
@@ -4,12 +4,14 @@ import { db } from "./db/db.js";
 const PORT = 3000
 
 //  Connect to database
-db.then(() => {
-    console.log('Connected to MySQL!')
-    //  Start express server
-    app.listen(PORT, () => {
-        console.log(`Server running on port: http://localhost:${PORT}`)
+db.query('SELECT 1')
+    .then(() => {
+        console.log('Connected to MySQL!')
+        //  Start express server
+        app.listen(PORT, () => {
+            console.log(`Server running on port: http://localhost:${PORT}`)
+        })
     })
-}).catch(err => {
-    console.error('DB connection error:', err)
-})
+    .catch(err => {
+        console.error('DB connection error:', err)
+    })
