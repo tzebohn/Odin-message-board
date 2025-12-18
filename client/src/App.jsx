7f6b4510a7e52bbf4b82832fc18dcc171ad2abc7
tzebohn <tzebohnling@gmail.com>
2025-12-18 11:20:55 -0500

    feat: Add reusable fetchPosts with pagination and scroll handling.
    - Add reusable fetchPosts function with pagination support
    - Fetch posts on component mount
    - Load older messages when scrolling to the top
    - Auto-scroll behavior for loading older messages vs new messages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import './App.css'
 import { IoIosAdd } from "react-icons/io";
 import MessageForm from './components/MessageForm';
@@ -11,37 +11,112 @@ function App() {
 
   const [showForm, setShowForm] = useState(false) // Dynamically displays MessageForm
   const [messages, setMessages] = useState([])    // Tracks all current messages
+  const [hasMore, setHasMore] = useState(true)    // Tracks if there are more messages
+  const [loading, setLoading] = useState(false)   // Tracks if currently fetching messages
+  const [page, setPage] = useState(1)             // Tracks the current page
 
-  const bottomRef = useRef(null)                  // Points to the current bottom of the message container
+  const bottomRef = useRef(null)            // Points to the current bottom of the message container
+  const hasMoreRef = useRef(true)           // Syncs with hasMore useState
+  const loadingRef = useRef(false)          // Syncs with loading useState
+  const loadedPagesRef = useRef(new Set())  // Keeps track of the pages currently loaded
+  const scrollModeRef = useRef("idle")      // Keeps track of how browser should handle automatic scrolling
+  const scrollContainerRef = useRef(null)   // References the message container
+  const prevScrollHeightRef = useRef(0)     // Stores the previous scroll height, before loading old messages
 
+  
+  // Sync refs with according react states
+  hasMoreRef.current = hasMore  // Tracks whether there are currently more pages to fetch
+  loadingRef.current = loading  // Tracks whether we are already currently fetching
+
+  /**
+   * Reusable GET function to fetch different pages of posts.
+   * 
+   * This function is only created once on initial component mount.
+   * 
+   * @param {Number} pageToLoad - The current page to fetch from database.
+   */
+  const fetchPosts = useCallback(async (pageToLoad = 1) => {
+    
+    // Check if page was already loaded before.
+    if (loadedPagesRef.current.has(pageToLoad)) {
+      return
+    }
+    
+    // Check if already loading, or no more messages to fetch
+    if (loadingRef.current || !hasMoreRef.current) return
+
+    try {
+      const response = await axios.get("/api/posts/get-posts", {
+        params: {
+          page: pageToLoad,
+          limit: 10
+        }
+      })
+
+      // Process the array of messages
+      const posts = response.data
+
+      // No messages to load, exit
+      if (posts.length === 0) {
+        hasMoreRef.current = false
+        setHasMore(false)
+        setLoading(false)
+        loadingRef.current = false
+        return
+      }
+
+      /**
+       * Valid messages, reverse the array so that most recent message
+       * displays at the bottom of the board.
+       */
+      posts.reverse()
+
+      // Update posts
+      setMessages(prev => {
+        const seen = new Set(prev.map(m => m.id))         // Get all of the current posts
+        const unique = posts.filter(p => !seen.has(p.id)) // Filter out only new posts
+        return [...unique, ...prev]
+      })
+      
+      // Update flags
+      loadedPagesRef.current.add(pageToLoad)
+      setPage(pageToLoad + 1)
+
+    } catch (err) {
+      console.error("Error fetching posts:", err)
+    } finally {
+      loadingRef.current = false
+      setLoading(false)
+    }
+  }, [])
 
   /**
    * Fetches the 25 most current posts on component mount.
    */
   useEffect(() => {
-    async function fetchPosts () {
-      try {
-        const response = await axios.get("/api/posts/get-posts")
-        const posts = response.data
-        posts.reverse()
-        setMessages(posts)
-      } catch (err) {
-        console.error("Error fetching posts:", err)
-      }
-    }
-
-    fetchPosts()
-  }, [])
+    fetchPosts(1)
+  }, [fetchPosts])
 
   /**
    * Updates bottomRef whenever messages state changes.
    * 
-   * Allows the message container to automatically scroll down
+   * Allows the message container to automatically scroll dynamically
    * when new messages are added.
    */
   useEffect(() => {
-    console.log("changing bottomref")
-    bottomRef.current?.scrollIntoView({ behavior: 'smooth' })
+    // Automatically scroll to the bottom when adding messages
+    if (scrollModeRef.current === "bottom" || scrollModeRef.current === "idle") {
+      bottomRef.current?.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    // Preverse the current position when loading previous old messages
+    if (scrollModeRef.current === "preserve") {
+      const container = scrollContainerRef.current
+      const newScrollHeight = container.scrollHeight
+      const heightDiff = newScrollHeight - prevScrollHeightRef.current
+
+      container.scrollTop = heightDiff
+    }
   }, [messages]) 
 
   /**
@@ -53,8 +128,12 @@ function App() {
   }
 
   /**
-   * Submits message form data to backend server.
-   * @param {Object} messageFormData 
+   * Submits message form data to backend server for validation.
+   * 
+   * Backend inserts into DB and returns a sanitized messageForm, if valid.
+   * Otherwise, returns error message if something goes wrong.
+   * 
+   * @param {Object} messageFormData - The username and message input to insert to database.
    */
   async function handleSubmit (messageFormData) {
     // Instantly display temp message on frontend for user
@@ -68,6 +147,7 @@ function App() {
     }
 
     setMessages(prev => [...prev, tempMessage])
+    scrollModeRef.current = "bottom"  // Automatically scroll to the bottom, when adding new message
 
     // Send POST request to backend server
     try {
@@ -114,6 +194,26 @@ function App() {
     }
   }
 
+  /**
+   * Called when user scrolls inside of message container.
+   * 
+   * Purpose of function is to load more messages when the user
+   * scrolls to the top of the container.
+   */
+  function handleScroll () {
+    const container = scrollContainerRef.current
+
+    if (!container) return
+    
+    // Try to load more messages when the user scrolls to the top
+    if (container.scrollTop === 0 && !loading && hasMore) {
+      scrollModeRef.current = "preserve"                    // Automatically stay at the scroll height, after loading old messages
+      prevScrollHeightRef.current = container.scrollHeight  // Get the current old height
+
+      fetchPosts(page)
+    }
+  }
+
   return (
     <main className='h-screen flex items-center justify-center bg-[#0F172A]'>
       <section className='relative flex flex-col border rounded-xl bg-[#1E293B] p-4 w-full max-w-[90%] sm:max-w-[500px] md:max-w-[600px] h-[700px]'>
@@ -122,7 +222,11 @@ function App() {
         <div className='border-t border-gray-600 my-2'></div>
 
         <div className='flex-1 flex flex-col gap-2 min-h-0'>
-          <div className='flex-1 p-2 overflow-y-auto'>
+          <div 
+            className='flex-1 p-2 overflow-y-auto'
+            onScroll={handleScroll}
+            ref={scrollContainerRef}
+          >
             {messages.map(msg => (
               <MessageBubble 
                 key={msg.id}
