2d642ffba55e0df59bc9c7ea0ed258d1426d8b69
tzebohn <tzebohnling@gmail.com>
2025-12-26 11:53:33 -0500

    fix: prepare deployment for socket client side

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client"
 
-const SOCKET_URL = "http://localhost:3000"
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL
 
 export const socket = io(SOCKET_URL, {
     autoConnect: false  // Prevents React Strict Mode from connecting twice on mount
